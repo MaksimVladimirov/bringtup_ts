24ef8c7494d0c05d6aa0bb6d17cd155a3e5fb030
Maksim Vladimirov <vl4dimirov@gmail.com>
2022-12-22 11:48:02 +0300

    bug

--- src/ts/modules/slider/slider.ts
+++ src/ts/modules/slider/slider.ts
@@ -1,18 +1,18 @@
 export class Slider {
   container: HTMLElement;
   slides: HTMLCollection;
-  btns: any;
-  prev: any;
-  next: any;
+  btns: NodeListOf<HTMLElement>;
+  prev: HTMLElement;
+  next: HTMLElement;
   slideIndex: number;
   activeClass: string;
   animate: boolean;
   autoplay: boolean;
   constructor({
     container = "",
-    btns = null,
-    next = null,
-    prev = null,
+    btns = "",
+    next = "",
+    prev = "",
     activeClass = "",
     animate = false,
     autoplay = false,
@@ -20,8 +20,8 @@ export class Slider {
     this.container = document.querySelector(container) as HTMLElement;
     this.slides = this.container.children;
     this.btns = document.querySelectorAll(btns);
-    this.next = document.querySelector(next);
-    this.prev = document.querySelector(prev);
+    this.next = document.querySelector(next) as HTMLElement;
+    this.prev = document.querySelector(prev) as HTMLElement;
     this.activeClass = activeClass;
     this.animate = animate;
     this.autoplay = autoplay;
